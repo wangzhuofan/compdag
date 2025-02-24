abdbae77fd3c7ed7704c0e66205b74f71ad175bc
Zhuofan Wanggit config --global user.email zf.wang@ruc.edu.cn <you@example.com>
2025-02-24 16:12:34 +0800

    first version

---
+++ R/compdag-package.R
@@ -0,0 +1,18 @@
+#' The 'compdag' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name compdag-package
+#' @aliases compdag
+#' @useDynLib compdag, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#' @importFrom rstantools rstan_config
+#' @importFrom RcppParallel RcppParallelLibs
+#'
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.26.23. https://mc-stan.org
+#'
+NULL
---
+++ R/compdag.R
@@ -0,0 +1,56 @@
+# compile stan file
+# library(rstan)
+
+# prepare data
+compdag <- function(x,y,paramx,paramy){
+  px = nrow(x)
+  py = nrow(y)
+  n = ncol(x)
+  #fit baseline
+  datax <- list(n=n,p1=px,y1=x)
+  fitx <- rstan::sampling(stanmodels$compDAG0,datax,iter=5000,chains=1,seed=1,thin = 20)
+  paramsx <- rstan::extract(fitx)
+  datay <- list(n=n,p1=py,y1=y)
+  fity <- rstan::sampling(stanmodels$compDAG0,datay,iter=5000,chains=1,seed=1,thin = 20)
+  paramsy <- rstan::extract(fity)
+
+  #fit regression
+  dataxy <- list(n=n,p1=px,p2=py,y1=x,y2=y,mu2=paramx)
+  fitxy <- rstan::sampling(stanmodels$compDAG1,dataxy,iter=5000,chains=1,seed=1,thin = 20)
+  paramsxy = rstan::extract(fitxy)
+  datayx <- list(n=n,p1=py,p2=px,y1=y,y2=x,mu2=paramy)
+  fityx <- rstan::sampling(stanmodels$compDAG1,datayx,iter=5000,chains=1,seed=1,thin = 20)
+  paramsyx = rstan::extract(fityx)
+
+  #calculate likelihood
+  loglx = paramsx$log_lik
+  logly = paramsy$log_lik
+  logxy = paramsxy$log_lik
+  logyx = paramsyx$log_lik
+
+  logx2y = loglx+logxy
+  logy2x = logly+logyx
+  logx0y = loglx+logly
+
+  x2yloo = rstan::loo(logx2y)$looic
+  y2xloo = rstan::loo(logy2x)$looic
+  x0yloo = rstan::loo(logx0y)$looic
+
+  # cd=1:causal direction is from x to y, cd=-1:causal direction is from y to x, cd=0:there is no causal relationship between y and x.
+  if (x2yloo==min(c(x2yloo,y2xloo,x0yloo)))
+    cd = 1
+  if (y2xloo==min(c(x2yloo,y2xloo,x0yloo)))
+    cd = -1
+  if (x0yloo==min(c(x2yloo,y2xloo,x0yloo)))
+    cd = 0
+
+  # get estimations
+  mEstxy <- apply(paramsxy$mt1,c(2,3),mean)
+  signalEstxy <- mean(paramsxy$pi1)
+  # mu1Estxy <- mean(paramsxy$a)
+  # calculate LOOIC for two directions
+
+
+  return(list(cd = cd,mEst = mEstxy,signalEst = signalEstxy,x2y=x2yloo,y2x=y2xloo,x0y = x0yloo))
+}
+
